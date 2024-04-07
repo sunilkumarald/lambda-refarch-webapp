e7be578fb743a9af869f23b70f8d4f5bb86235e4
SK <suniel.vns@gmail.com>
2024-04-07 14:54:55 +0530

    Update config.default.js

--- www/src/config.default.js
+++ www/src/config.default.js
@@ -3,9 +3,9 @@
 
 const config = {
   "aws_user_pools_web_client_id": "AWS_USER_POOLS_WEB_CLIENT_ID",     // CognitoClientID
-  "api_base_url": "API_BASE_URL",                                     // TodoFunctionApi
+  "api_base_url": "http://127.0.0.1:8080",                                     // TodoFunctionApi
   "cognito_hosted_domain": "COGNITO_HOSTED_DOMAIN",                   // CognitoDomainName
-  "redirect_url": "REDIRECT_URL"                                      // AmplifyURL
+  "redirect_url": "https://localhost:8080"                                      // AmplifyURL
 };
 
 export default config;
