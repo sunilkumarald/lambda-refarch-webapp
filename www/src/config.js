bc5135878576c725eb18103388d9450d7f8bb506
SK <suniel.vns@gmail.com>
2024-04-07 14:51:32 +0530

    Update config.js

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,28 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
-};
+        "OutputKey": "CognitoClientID",
+        "OutputValue": "65scsb3o6c1fbf1aaip1h4vrs6",
+        "Description": "The Cognito UserPool Client ID"
+    },
+    {
+        "OutputKey": "CognitoDomainName",
+        "OutputValue": "mytodoappdemo-sam-app.auth.ap-south-1.amazoncognito.com",
+        "Description": "The Cognito Hosted UI Domain Name"
+    },
+    {
+        "OutputKey": "AmplifyURL",
+        "OutputValue": "https://master.d1rq0pm0cfagwz.amplifyapp.com"
+    },
+    {
+        "OutputKey": "CognitoID",
+        "OutputValue": "ap-south-1_7KD7TSCvy",
+        "Description": "The Cognito UserPool ID"
+    },
+    {
+        "OutputKey": "TodoFunctionApi",
+        "OutputValue": "https://rn6y5v0t29.execute-api.ap-south-1.amazonaws.com/{StageNameParam}",
+        "Description": "API Gateway endpoint URL for Prod stage"
+    };
 
 export default config;
